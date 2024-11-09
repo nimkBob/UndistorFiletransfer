206dbc293a0acea8e8dd9694bca769be0bcd7b02
nimkBob <lizhanmeng48@gmail.com>
2024-11-09 12:19:36 +0800

    three

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
+include '/feature/Sign-up.php';
+feedback("cao");
 /* if($_SERVER['REQUEST_METHOD']=='POST')
   $featureType=$_POST['featureType'];
   switch($featureType){
     case "Login":
-     include 'Login.php';
+     include '/feature/Login.php';
      break;
     case "Sign-up":
-     include 'Sign-up.php';
+     include '/feature/Sign-up.php';
      break;
     case "File-operation":
-     include 'File-operation.php';
+     include '/feature/File-operation.php';
      break;
     case "Outside-interface":
-     include 'Outside-interface.php';
+     include '/feature/Outside-interface.php';
      break;
     case "User-management":
-     include 'User-management.php';
+     include '/feature/User-management.php';
     default:
     $feedback=['feedbackstate'=>'error:featureTypet-is-error'];
     header('Content-Type:application/json');
