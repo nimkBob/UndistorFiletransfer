543b6df32fd8280c01676a8a89013bb3f77ba758
nimkBob <lizhanmeng48@gmail.com>
2024-11-11 15:51:57 +0800

    five

---
+++ feature/check.php
@@ -0,0 +1,38 @@
+<?php
+ include 'Mysql.php'; 
+ function isAlphanumeric($str){
+    for($i=0;$i<strlen($str);$i++){
+        $char=$str[$i];
+        if(!(ctype_alnum($char))){
+            return false;
+        }
+    }
+    return true;
+ }
+
+ function validate($value,$maxlen,$minlen){//检验用户上传数据的合法性
+    //注册验证用户的输入是否合法
+    if(!(isAlphanumeric($value)) and $value==null){
+      feedback("error:Characters-do-not-meet-requirements");
+      return false;
+   }
+    
+    if(strlen($value)>$maxlen || strlen($value)<$minlen){
+      feedback("error:The-length-does-not-meet-the-requirements");
+      return false;
+    }
+    return true;
+  }
+
+  function isExist($name){
+    //检测该用户账号是否已存在
+    include "Mysql.php";
+    $feedbackValue = new Sql();
+    if($feedbackValue->checkExistence($name, 'username', 'user')){
+      feedback("error:The-named-user-already-exists");
+      return false;
+    }
+    return true;
+  }
+
+?>
